deb4c50a59594d12db4a476009ba6f0fb365652b
Max-Gording <akim.gendel@gmail.com>
2016-06-19 22:04:24 +0200

    Changes проверяем isInAppBrowserInstalled() 190616 2205

--- www/js/index.js
+++ www/js/index.js
@@ -39,7 +39,7 @@ var app = {
     // deviceready Event Handler
 
     onDeviceReady: function() {
-
+        alert("Проверяем, есть ли плагин InAppBrouser " + isInAppBrowserInstalled());
         window.open = window.cordova.InAppBrowser.open;
         /*    function onOffline(){
          app.goingState = "no-internet";
